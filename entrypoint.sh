8715a26d24724f8f424fb1f7e5994fac24beca70
HappySR <darrang48@gmail.com>
2025-04-04 20:27:01 +0530

    Changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 set -ex
 
 if [ ! -d "/root/.local/share/argos-translate/packages" ]; then
-  libretranslate --update_models  # <-- Fix hyphen to underscore
-  libretranslate --install_lang en || true  # <-- Fix hyphen to underscore
-  libretranslate --install_lang hi || true
-  libretranslate --install_lang es || true
-  libretranslate --install_lang fr || true
-  libretranslate --install_lang de || true
-  libretranslate --install_lang zh || true
-  libretranslate --install_lang ja || true
-  libretranslate --install_lang bn || true
-  libretranslate --install_lang ar || true
+  libretranslate --update-models  # <-- Hyphen here
+  libretranslate --install-lang en || true  # <-- Hyphen here
+  libretranslate --install-lang hi || true
+  libretranslate --install-lang es || true
+  libretranslate --install-lang fr || true
+  libretranslate --install-lang de || true
+  libretranslate --install-lang zh || true
+  libretranslate --install-lang ja || true
+  libretranslate --install-lang bn || true
+  libretranslate --install-lang ar || true
 fi
 
 exec libretranslate --host 0.0.0.0 --port ${PORT:-5000}
